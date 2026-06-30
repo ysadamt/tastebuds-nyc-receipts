d6f801c11b0cf44a4d7bc2c32d497bf508d03694
ysadamt <adam.teo321@gmail.com>
2026-06-29 21:24:03 -0500

    add bangladesh

--- app/page.tsx
+++ app/page.tsx
@@ -49,8 +49,8 @@ const COORDS: Record<string, [number, number]> = {
   GY: [5.0, -59.0],
   RU: [60.0, 100.0],
   DZ: [28.0, 3.0],
-  IN: [20.6, 78.9],
   CO: [4.0, -72.0],
+  BD: [24.0, 90.0],
 };
 
 type ReceiptSection = { section: string; items: string[] };
